017deba7a270db5ae68a0415e65422a57427f469
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-07-23 20:13:39 +0100

    Add multiple language support to greeting

--- learn-go-with-pocket-sized-projects/01_hello_earth/main.go
+++ learn-go-with-pocket-sized-projects/01_hello_earth/main.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	greeting := greet()
+	greeting := greet("EN")
 	fmt.Println(greeting)
 }
 
+// Language represents a language's two character code
+type language string
+
 // Return a greeting
-func greet() string {
-	return "Hello, world!"
+func greet(lang language) string {
+	switch lang {
+	case "EN":
+		return "Hello, world!"
+	case "FR":
+		return "Bonjour, le monde!"
+	default:
+		return ""
+	}
 }
--- learn-go-with-pocket-sized-projects/01_hello_earth/main_test.go
+++ learn-go-with-pocket-sized-projects/01_hello_earth/main_test.go
@@ -8,9 +8,30 @@ func Example() {
 	// Hello, world!
 }
 
-func TestGreeting(t *testing.T) {
+func TestGreet_EN(t *testing.T) {
+	lang := language("EN")
 	want := "Hello, world!"
-	got := greet()
+	got := greet(lang)
+
+	if want != got {
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_FR(t *testing.T) {
+	lang := language("FR")
+	want := "Bonjour, le monde!"
+	got := greet(lang)
+
+	if want != got {
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_Default(t *testing.T) {
+	lang := language("FOO")
+	want := ""
+	got := greet(lang)
 
 	if want != got {
 		t.Errorf("expected: %q, got: %q", want, got)
