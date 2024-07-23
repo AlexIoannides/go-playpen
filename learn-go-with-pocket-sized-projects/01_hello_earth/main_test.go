3490328da7cb11044b85022f245fac9a8c8d2001
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-07-23 19:34:41 +0100

    Refactor message into generic greeting function

--- learn-go-with-pocket-sized-projects/01_hello_earth/main_test.go
+++ learn-go-with-pocket-sized-projects/01_hello_earth/main_test.go
@@ -1,7 +1,18 @@
 package main
 
+import "testing"
+
 func Example() {
 	main()
 	// Output:
 	// Hello, world!
+}
+
+func TestGreeting(t *testing.T) {
+	want := "Hello, world!"
+	got := greet()
+
+	if want != got {
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
 }
