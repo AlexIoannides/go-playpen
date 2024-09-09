ff22a78edf09921deffdf633385ead131b670c1d
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-09-09 18:02:16 +0100

    Add constructor for set type

--- learn-go-with-pocket-sized-projects/02-bookworm-digest/recommendations.go
+++ learn-go-with-pocket-sized-projects/02-bookworm-digest/recommendations.go
@@ -2,7 +2,27 @@ package main
 
 type set map[Book]struct{}
 
+// Does set contain book?
 func (s set) Contains(b Book) bool {
 	_, ok := s[b]
 	return ok
+}
+
+// Create set of Books from list of Books
+func newSet(books *[]Book) *set {
+	bookSet := make(set)
+	for _, book := range *books {
+		bookSet[book] = struct{}{}
+	}
+	return &bookSet
+}
+
+type Recommendation struct {
+	Book  Book
+	Score float64
+}
+
+func recommend(allBookworms []Bookworm, targetBookworm Book, n int) []Recommendation {
+
+	return make([]Recommendation, 0)
 }
--- learn-go-with-pocket-sized-projects/02-bookworm-digest/recommendations_test.go
+++ learn-go-with-pocket-sized-projects/02-bookworm-digest/recommendations_test.go
@@ -4,10 +4,37 @@ import "testing"
 
 func TestSet_Contains(t *testing.T) {
 	s := set{handmaidsTale: {}, oryxAndCrake: {}, theBellJar: {}}
-	if !s.Contains(theBellJar) {
-		t.Fatalf("expected set to contain book: %v", theBellJar)
+	t.Run("contains_matching_book", func(t *testing.T) {
+		if !s.Contains(theBellJar) {
+			t.Errorf("expected set to contain book: %v", theBellJar)
+		}
+	})
+	t.Run("contains_no_matching_book", func(t *testing.T) {
+		if s.Contains(janeEyre) {
+			t.Errorf("expected set not to contain book: %v", janeEyre)
+		}
+	})
+}
+
+func TestNewSet(t *testing.T) {
+	bookList := []Book{handmaidsTale, handmaidsTale, oryxAndCrake}
+	bookSet := *newSet(&bookList)
+
+	if len(bookSet) != 2 {
+		t.Fatal("expected set to have length 2")
 	}
-	if s.Contains(janeEyre) {
-		t.Fatalf("expected set not to contain book: %v", janeEyre)
+
+	_, ok := bookSet[handmaidsTale]
+	if !ok {
+		t.Fatalf("expected book %v to be in set", handmaidsTale)
 	}
+
+	_, ok = bookSet[oryxAndCrake]
+	if !ok {
+		t.Fatalf("expected book %v to be in set", oryxAndCrake)
+	}
+}
+
+func TestRecommend(t *testing.T) {
+
 }
