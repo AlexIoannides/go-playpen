a0c228b8b8f79302ba798997cee16ec6d4592736
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-09-11 19:20:05 +0100

    Add external test

---
+++ learn-go-with-pocket-sized-projects/03-log-story/logger/pocketlog/logger_test.go
@@ -0,0 +1,9 @@
+package pocketlog_test
+
+import "alexioannides/go-playpen/learn-go-with-pocket-sized-projects/03-log-story/logger/logger/pocketlog"
+
+func ExampleLogger_Debugf() {
+	debugLogger := pocketlog.New(pocketlog.LevelDebug)
+	debugLogger.Debugf("Hello, %s", "world!")
+	// Output: Hello, world!
+}
