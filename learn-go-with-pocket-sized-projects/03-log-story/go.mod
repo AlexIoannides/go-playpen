6f9fbc1ec51cdd74df6b36ba1cc085a3fb489b88
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-09-11 17:09:11 +0100

    Define logging API

---
+++ learn-go-with-pocket-sized-projects/03-log-story/go.mod
@@ -0,0 +1,3 @@
+module alexioannides/go-playpem/learn-go-with-pocket-sized-projects/03-log-story/logger
+
+go 1.22.5
---
+++ learn-go-with-pocket-sized-projects/03-log-story/logger/pocketlog/logger.go
@@ -0,0 +1,28 @@
+package pocketlog
+
+// Logger is used to log information
+type Logger struct {
+	threshold Level
+}
+
+// New returns a lodder, ready to log at the required level
+func New(threshold Level) *Logger {
+	return &Logger{
+		threshold: threshold,
+	}
+}
+
+// Debugf formats and logs a message if the log level is LevelDebug
+func (l *Logger) Debugf(format string, args ...any) {
+	// TODO
+}
+
+// Infof formats and logs a message if the log level is LevelInfo
+func (l *Logger) Infof(format string, args ...any) {
+	// TODO
+}
+
+// Errorf formats and logs a message if the log level is LevelError
+func (l *Logger) Errorf(format string, args ...any) {
+	// TODO
+}
