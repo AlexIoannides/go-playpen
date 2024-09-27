82b4811c23e40082b4e178665c259bd65de2b244
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-09-27 10:25:31 +0100

    Fix typo

--- learn-go-with-pocket-sized-projects/07-gordle-as-a-service/internal/handlers/router.go
+++ learn-go-with-pocket-sized-projects/07-gordle-as-a-service/internal/handlers/router.go
@@ -19,9 +19,9 @@ func NewRouter(db *repository.GameRepository) *http.ServeMux {
 	r := http.NewServeMux()
 
 	// Register each endpoint.
-	r.HandleFunc(http.MethodPost+" "+api.NewGameRoute, newgame.Handler(db))    // curl -X POST -v http://localhost:9090/games
-	r.HandleFunc(http.MethodGet+" "+api.GetStatusRoute, getstatus.Handler(db)) // curl -X GET -v http://localhost:9090/games/1682279480
-	r.HandleFunc(http.MethodPut+" "+api.GuessRoute, guess.Handler(db))         // curl -X PUT -v http://localhost:9090/games/1682279480 -d '{"guess":"faune"}'
+	r.HandleFunc(http.MethodPost+" "+api.NewGameRoute, newgame.Handler(db))    // curl -X POST -v http://localhost:8080/games
+	r.HandleFunc(http.MethodGet+" "+api.GetStatusRoute, getstatus.Handler(db)) // curl -X GET -v http://localhost:8080/games/1682279480
+	r.HandleFunc(http.MethodPut+" "+api.GuessRoute, guess.Handler(db))         // curl -X PUT -v http://localhost:8080/games/1682279480 -d '{"guess":"faune"}'
 
 	return r
 }
